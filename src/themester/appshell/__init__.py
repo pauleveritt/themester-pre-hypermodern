6afd58ae4048f759d050617311e3755233d5738f
Paul Everitt <pauleveritt@me.com>
2020-04-05 15:34:31 -0400

    Start at a base decorator, copied from wired_component

---
+++ src/themester/appshell/__init__.py
@@ -0,0 +1,17 @@
+"""
+
+The AppShell provides the structure for a theme to pour into.
+
+- Multiple appshells can be registered
+- They
+
+"""
+
+from zope.interface import Interface
+
+
+class IAppShell(Interface):
+    pass
+
+
+
---
+++ src/themester/base_decorator.py
@@ -0,0 +1,44 @@
+"""
+
+Make it easy to write entity-specific decorators.
+
+"""
+from typing import Type, Callable
+
+from venusian import Scanner
+from wired import ServiceRegistry
+from wired.dataclasses import register_dataclass
+
+
+class BaseDecorator:
+    """ Subclass this to fill in the slots """
+
+    for_: Type
+    register_function: Callable
+
+    def __init__(
+            self, for_: Type = None, context: Type = None, name: str = None
+    ):
+        # Some decorators (e.g. resource) require a ``for_`` in their
+        # usage, as one decorator covers lots of classes. Others
+        # (e.g. ``view``) provide the ``for_`` once, as a a class attribute.
+        self.for_ = for_ if for_ else self.for_
+        self.context = context
+        self.name = name
+
+    def __call__(self, wrapped):
+        from venusian import attach
+
+        def callback(scanner: Scanner, name: str, cls):
+            registry: ServiceRegistry = getattr(scanner, 'registry')
+
+            register_dataclass(
+                registry,
+                cls,
+                for_=self.for_,
+                context=self.context,
+                # name=self.name,
+            )
+
+        attach(wrapped, callback, category='themester')
+        return wrapped
