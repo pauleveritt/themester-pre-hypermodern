97b7b54484a9e6162dbd3406e47c0388bbf48064
Paul Everitt <pauleveritt@me.com>
2020-05-13 09:29:25 -0400

    Minimum start to builder/inject.

--- src/themester/sphinx.py
+++ src/themester/sphinx.py
@@ -1,34 +1,73 @@
-import inspect
-import os
+from dataclasses import dataclass
 
 from sphinx.application import Sphinx
+from sphinx.jinja2glue import BuiltinTemplateLoader
+from viewdom import html
+from viewdom_wired import render
+from wired import ServiceRegistry, ServiceContainer
+from wired.dataclasses import register_dataclass, injected, Context
+
+from themester import View
+from themester.renderer import Renderer, VDOMRenderer
+from themester.resources import Resource, Root
+
+
+@dataclass
+class DummyView:
+    # resource: Resource = injected(Context)
+    container: ServiceContainer
+
+    def __call__(self):
+        name = self.container.context.name
+        return html('<div>Hello {name}</div>')
+
+
+class ThemesterBridge(BuiltinTemplateLoader):
+
+    def render(self, template, context):
+        container: ServiceContainer = context['container']
+        view = container.get(View)
+        vdom = view()
+        response = render(vdom, container=container)
+        return response
+
+
+def builder_init(app: Sphinx):
+    """ Wire up some global stuff after Sphinx startup """
+
+    # Make a registry and store in Sphinx environment
+    registry = ServiceRegistry()
+    app.wired_registry = registry
+
+    # Resource tree with a root and one document
+    root = Root()
+    f1 = Resource(name='f1', parent=root)
+    root['f1'] = f1
+    registry.register_singleton(root, Root)
+    app.wired_root = root
+
+    # Register a view and renderer
+    register_dataclass(registry, DummyView, View, context=Resource)
+    register_dataclass(registry, VDOMRenderer, Renderer)
 
 
 def inject_page(app, pagename, templatename, context, doctree):
-    return 'page2.html'
+    """ Store a resource-bound container in Sphinx context """
 
-
-def add_template_dir(app: Sphinx):
-    """ Called on builderinit, let's add the template subdir """
-
-    # Usually Sphinx themes put their templates in the root,
-    # where the theme.conf and __init__.setup reside. Let's
-    # have our templates in a templates dir.
-
-    template_bridge = app.builder.templates
-
-    import themester
-    template_dir = os.path.join(os.path.dirname(inspect.getfile(themester)),
-                                'templates')
-    template_bridge.loaders[1].searchpath.append(template_dir)
-    return
+    registry: ServiceRegistry = app.wired_registry
+    root: Root = app.wired_root
+    resource = root['f1']
+    container = registry.create_container(context=resource)
+    context['container'] = container
 
 
 def setup(app: Sphinx):
-    # Normal Sphinx theme-wiring
-    f = os.path.abspath(os.path.dirname(__file__))
-    app.add_html_theme('themester', f)
-    app.connect('builder-inited', add_template_dir)
+    app.connect('builder-inited', builder_init)
+
+    # Not using any of the Sphinx HTML builder machinery
+    # nor Jinja2
+    bridge = 'themester.sphinx.ThemesterBridge'
+    app.config.template_bridge = bridge
 
     app.connect('html-page-context', inject_page)
 
