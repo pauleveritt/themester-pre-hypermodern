c8458dc7e275c5f609167448e8c5fd2ec5a6ecc8
Paul Everitt <pauleveritt@me.com>
2020-07-25 10:45:24 -0400

    Start using a ThemabasterConfig file in the test.

--- tests/sphinx/roots/test-setup/conf.py
+++ tests/sphinx/roots/test-setup/conf.py
@@ -1,6 +1,6 @@
 import sys
 
-from themester.testing.config import ThemesterConfig
+from themester.themabaster.config import ThemabasterConfig
 
 extensions = ['themester']
 
@@ -10,4 +10,11 @@ html_title = ''
 exclude_patterns = ['_build']
 current_module = sys.modules[__name__]
 
-themester_config = ThemesterConfig(site_name='Test Setup')
+# themester_config = ThemesterConfig(site_name='Test Setup')
+themester_config = ThemabasterConfig(
+    css_files=('site_first.css', 'site_second.css',),
+    favicon='themabaster.ico',
+    logo='site_logo.png',
+    site_name='Themester SiteConfig',
+    touch_icon='sometouchicon.ico'
+)
