67788bf17e36c4cde199edf48538c5b4462b5aa3
Paul Everitt <pauleveritt@me.com>
2020-07-26 10:42:58 -0400

    Document all the Sphinx core config values.

--- src/themester/config.py
+++ src/themester/config.py
@@ -16,53 +16,53 @@ from typing import Optional, Dict, List
 class SphinxConfig:
     """ Config values of Sphinx, as a typed dataclass """
 
-    project: str = 'Python'
-    author: Optional[str] = None
-    copyright: Optional[str] = None
-    version: Optional[str] = None
-    release: Optional[str] = None
+    # figure_language_filename
+
+    source_parsers: Dict = field(default_factory=lambda: {})
+    source_suffix: Dict[str, str] = field(default_factory=lambda: {'.rst': 'restructuredtext'})
+    suppress_warnings: List = field(default_factory=lambda: [])
+    template_bridge: Optional[str] = None
+    templates_path: List = field(default_factory=lambda: [])
+    tls_cacerts: Optional[List] = field(default_factory=lambda: [])
+    tls_verify: bool = True
     today: Optional[str] = None
     today_fmt: Optional[str] = None
-    language: Optional[str] = None
-    locale_dirs: List[str] = field(default_factory=lambda: ['locales', ])
-    # figure_language_filename
-    master_doc: str = 'index'
-    source_suffix: Dict[str, str] = field(default_factory=lambda: {'.rst': 'restructuredtext'})
-    source_encoding: str = 'utf-8-sig'
-    source_parsers: Dict = field(default_factory=lambda: {})
-    exclude_patterns: List = field(default_factory=lambda: [])
-    default_role: Optional[str] = None
+    trim_doctest_flags: bool = True
+    trim_footnote_reference_space: bool = False
+    user_agent: Optional[str] = None
+    version: Optional[str] = None
     add_function_parentheses: bool = True
     add_module_names: bool = True
-    trim_footnote_reference_space: bool = False
-    show_authors: bool = False
-    pygments_style: List[str] = field(default_factory=lambda: [])
+    author: Optional[str] = None
+    copyright: Optional[str] = None
+    default_role: Optional[str] = None
+    exclude_patterns: List = field(default_factory=lambda: [])
     highlight_language: str = 'default'
     highlight_options: Dict = field(default_factory=lambda: {})
-    templates_path: List = field(default_factory=lambda: [])
-    template_bridge: Optional[str] = None
     keep_warnings: bool = False
-    suppress_warnings: List = field(default_factory=lambda: [])
+    language: Optional[str] = None
+    locale_dirs: List[str] = field(default_factory=lambda: ['locales', ])
+    manpages_url: Optional[str] = None
+    master_doc: str = 'index'
+    math_eqref_format: Optional[str] = None
+    math_number_all: bool = False
+    math_numfig: bool = True
     modindex_common_prefix: List = field(default_factory=lambda: [])
+    needs_extensions: Dict = field(default_factory=lambda: {})
+    needs_sphinx: Optional[str] = None
+    nitpick_ignore: List[str] = field(default_factory=lambda: [])
+    nitpicky: bool = False
+    numfig: bool = False
+    numfig_format: Dict = field(default_factory=lambda: {})
+    numfig_secnum_depth: int = 1
+    primary_domain: str = 'py'
+    project: str = 'Python'
+    pygments_style: List[str] = field(default_factory=lambda: [])
+    release: Optional[str] = None
     rst_epilog: Optional[str] = None
     rst_prolog: Optional[str] = None
-    trim_doctest_flags: bool = True
-    primary_domain: str = 'py'
-    needs_sphinx: Optional[str] = None
-    needs_extensions: Dict = field(default_factory=lambda: {})
-    manpages_url: Optional[str] = None
-    nitpicky: bool = False
-    nitpick_ignore: List[str] = field(default_factory=lambda: [])
-    numfig: bool = False
-    numfig_secnum_depth: int = 1
-    numfig_format: Dict = field(default_factory=lambda: {})
-    math_number_all: bool = False
-    math_eqref_format: Optional[str] = None
-    math_numfig: bool = True
-    tls_verify: bool = True
-    tls_cacerts: Optional[List] = field(default_factory=lambda: [])
-    user_agent: Optional[str] = None
+    show_authors: bool = False
     smartquotes: bool = True
     smartquotes_action: str = 'qDe'
-    smartquotes_excludes: Dict[str, str] = field(default_factory=lambda: {'languages': ['ja'],
-                                                                          'builders': ['man', 'text']})
+    smartquotes_excludes: Dict[str, str] = field(default_factory=lambda: {'languages': ['ja']})
+    source_encoding: str = 'utf-8-sig'
