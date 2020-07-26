7783a1c16e3fec228f376bbe0cafc81fdec9c597
Paul Everitt <pauleveritt@me.com>
2020-07-26 14:43:51 -0400

    Omit more of the Sphinx conf values.

--- tests/sphinx/roots/test-setup/conf.py
+++ tests/sphinx/roots/test-setup/conf.py
@@ -1,15 +1,6 @@
-import sys
-
 from themester.themabaster.config import ThemabasterConfig
 
 extensions = ['themester', 'themester.themabaster']
-
-# The rest is normal Sphinx stuff
-master_doc = 'index'
-html_title = ''
-exclude_patterns = ['_build']
-current_module = sys.modules[__name__]
-
 
 theme_config = ThemabasterConfig(
     css_files=('site_first.css', 'site_second.css',),
