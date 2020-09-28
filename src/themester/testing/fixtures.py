677987661f7080ca18d8051a6860d8e4f669a590
Paul Everitt <pauleveritt@me.com>
2020-09-28 18:06:45 -0400

    Clean up some fixtures.

--- src/themester/testing/fixtures.py
+++ src/themester/testing/fixtures.py
@@ -109,58 +109,63 @@ def this_html(this_vdom) -> BeautifulSoup:
     return this_html
 
 
+def fake_pathto(docname, mode=0) -> str:
+    """ Sphinx page context function to get a path to a target """
+    return f'../mock/{docname}'
+
+
 @pytest.fixture
 def this_pathto() -> Callable[[str, Optional[int]], str]:
-    def _this_pathto(docname, mode=0) -> str:
-        """ Sphinx page context function to get a path to a target """
-        return f'../mock/{docname}'
+    return fake_pathto
 
-    return _this_pathto
+
+def fake_hasdoc(docname) -> bool:
+    """ Sphinx page context function to confirm a path exists """
+    return True if docname != 'author' else False
 
 
 @pytest.fixture
 def this_hasdoc() -> Callable[[str], bool]:
-    def _this_hasdoc(docname) -> bool:
-        """ Sphinx page context function to confirm a path exists """
-        return True if docname != 'author' else False
+    return fake_hasdoc
 
-    return _this_hasdoc
+
+def fake_toctree(sidebar_collapse: bool = True, sidebar_includehidden: bool = True) -> str:
+    """ Sphinx page context function to return string of toctree """
+    return '<ul><li>First</li></ul>'
 
 
 @pytest.fixture
 def this_toctree() -> Callable[[bool, bool], str]:
-    def _this_toctree(sidebar_collapse: bool = True, sidebar_includehidden: bool = True) -> str:
-        """ Sphinx page context function to return string of toctree """
-        return '<ul><li>First</li></ul>'
+    return fake_toctree
 
-    return _this_toctree
+
+fake_pagecontext = PageContext(
+    body=Markup('<h1>Some Body</h1>'),
+    css_files=('page_first.css', 'page_second.css'),
+    display_toc=True,
+    hasdoc=fake_hasdoc,
+    js_files=('page_first.js', 'page_second.js'),
+    pagename='somedoc',
+    page_source_suffix='.html',
+    pathto=fake_pathto,
+    prev=Link(
+        title='Previous',
+        link='/previous/',
+    ),
+    next=Link(
+        title='Next',
+        link='/next/',
+    ),
+    sourcename='somedoc.rst',
+    title='Some Page',
+    toc=Markup('<li>toc</li>'),
+    toctree=fake_toctree,
+)
 
 
 @pytest.fixture
 def this_pagecontext(this_hasdoc, this_pathto, this_toctree):
-    pc = PageContext(
-        body=Markup('<h1>Some Body</h1>'),
-        css_files=('page_first.css', 'page_second.css'),
-        display_toc=True,
-        hasdoc=this_hasdoc,
-        js_files=('page_first.js', 'page_second.js'),
-        pagename='somedoc',
-        page_source_suffix='.html',
-        pathto=this_pathto,
-        prev=Link(
-            title='Previous',
-            link='/previous/',
-        ),
-        next=Link(
-            title='Next',
-            link='/next/',
-        ),
-        sourcename='somedoc.rst',
-        title='Some Page',
-        toc=Markup('<li>toc</li>'),
-        toctree=this_toctree,
-    )
-    return pc
+    return fake_pagecontext
 
 
 @pytest.fixture
