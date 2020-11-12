1622d5befaedc81a6aba99e88aba9530e4cc21b9
Paul Everitt <pauleveritt@me.com>
2020-11-12 14:53:35 -0500

    CanonicalLink

--- tests/themabaster/components/test_canonical_link.py
+++ tests/themabaster/components/test_canonical_link.py
@@ -1,63 +1,26 @@
-import dataclasses
+from typing import Tuple
 
 import pytest
-from bs4 import BeautifulSoup
-from viewdom import html
-from viewdom_wired import render
 
-from themester.sphinx import HTMLConfig
-from themester.themabaster.components.canonical_link import CanonicalLink
+from themester.storytime import Story
+from themester.themabaster.components import canonical_link
 
 
-@pytest.fixture
-def this_props(sphinx_config, html_config, this_pagecontext):
-    props = dict(
-        baseurl='https://somewhere.com/mysite',
-        file_suffix=html_config.file_suffix,
-        pagename=this_pagecontext.pagename,
-    )
-    return props
-
-
-@pytest.fixture
-def this_component(this_props):
-    ci = CanonicalLink(**this_props)
-    return ci
-
-
-def test_construction(this_component: CanonicalLink):
-    assert 'https://somewhere.com/mysite/somedoc.html' == this_component.canonical_href
-
-
-def test_vdom(this_vdom):
-    assert 'canonical' == this_vdom.props['rel']
-    assert 'https://somewhere.com/mysite/somedoc.html' == this_vdom.props['href']
-
-
-def test_no_baseurl(this_props):
-    this_props['baseurl'] = None
-    ci = CanonicalLink(**this_props)
-    expected = 'https://somewhere.com/mysite/somedoc.html'
-    assert None is ci.canonical_href
-    vdom = ci()
-    assert None is vdom
-
-
-def test_render(this_html):
-    link = this_html.select_one('link')
+@pytest.mark.parametrize('component_package', (canonical_link,))
+def test_stories(these_stories: Tuple[Story, ...]):
+    story0 = these_stories[0]
+    assert 'https://somewhere.com/mysite/somedoc.html' == story0.instance.canonical_href
+    assert 'canonical' == story0.vdom.props['rel']
+    link = story0.html.select_one('link')
     assert 'canonical' == link.get('rel')[0]
     assert 'https://somewhere.com/mysite/somedoc.html' == link.get('href')
 
+    story1 = these_stories[1]
+    expected = 'https://somewhere.com/mysite/somedoc.html'
+    assert None is story1.instance.canonical_href
+    assert None is story1.vdom
 
-def test_wired_render(this_container, html_config):
-    sc = dataclasses.replace(
-        html_config,
-        baseurl='https://somewhere.com/mysite',
-    )
-    this_container.register_singleton(sc, HTMLConfig)
-    this_vdom = html('<{CanonicalLink} />')
-    rendered = render(this_vdom, container=this_container)
-    this_html = BeautifulSoup(rendered, 'html.parser')
-    link = this_html.select_one('link')
+    story2 = these_stories[2]
+    link = story2.html.select_one('link')
     assert 'canonical' == link.get('rel')[0]
     assert 'https://somewhere.com/mysite/somedoc.html' == link.get('href')
